b461391dd47fc7b2b375d88baab33a6dccaaca33
githubwonder1128 <crazywonderrabbit1128@gmail.com>
2022-07-15 03:05:25 -0700

    Man_power

--- resources/views/readonly/manpower_readonly.blade.php
+++ resources/views/readonly/manpower_readonly.blade.php
@@ -131,7 +131,7 @@
                                 <td class="p_ic" data-text="{{$Projects[$i]['p_ic']}}">{{$Projects[$i]['p_ic']}}</td>
                                 <td class="include_materials" data-text="{{$Projects[$i]['include_materials']}}">{{$Projects[$i]['include_materials']}}</td>
                                 <td class="p_award" data-text="{{$Projects[$i]['p_award']}}">{{$Projects[$i]['p_award']}}</td>
-                                <td class="f_completion" data-text="{{$Projects[$i]['f_completion']}}">{{$Projects[$i]['p_completion']}}</td>
+                                <td class="f_completion" data-text="{{$Projects[$i]['f_completion']}}">{{$Projects[$i]['f_completion']}}</td>
                     
                             </tr>
                             @endfor
@@ -149,26 +149,26 @@
                 <div class="row">
                     <table class="table mdl-data-table" id="table_hours" style="width: 100%">
                         <thead>
-                            <tr>
-                                <th>P.Code</th>
-                                <th>P.Name</th>
-                                <th>CompanyName</th>
-                                <th>Date</th>
-                                <th>Worker Name</th>
-                                <th>Worker ID</th>
-                                <th>Hours</th>
+                            <tr id="table_man_power-0">
+                                <th class="p_code-header">P.Code</th>
+                                <th class="p_name-header">P.Name</th>
+                                <th class="company_name-header">CompanyName</th>
+                                <th class="man_date-header">Date</th>
+                                <th class="worker_name-header">Worker Name</th>
+                                <th class="worker_key-header">Worker ID</th>
+                                <th class="man_hours-header">Hours</th>
                             </tr>
                         </thead>
                         <tbody>
                             @for($i = 0; $i < count($Man_power); $i++)
-                                <tr>
-                                    <td>{{$Man_power[$i]->p_code}}</td>
-                                    <td>{{$Man_power[$i]->p_name}}</td>
-                                    <td>{{$Man_power[$i]->company_name}}</td>
-                                    <td>{{$Man_power[$i]->man_date}}</td>
-                                    <td>{{$Man_power[$i]->worker_name}}</td>
-                                    <td>{{$Man_power[$i]->worker_key}}</td>
-                                    <td>{{$Man_power[$i]->man_hours}}</td>
+                                <tr id="table_man_power-{{$Man_power[$i]->man_id}}" ondblclick="editRow(id)">
+                                    <td class="p_code" data-text="{{$Man_power[$i]->p_code}}">{{$Man_power[$i]->p_code}}</td>
+                                    <td class="p_name" data-text="{{$Man_power[$i]->p_name}}">{{$Man_power[$i]->p_name}}</td>
+                                    <td class="company_name" data-text="{{$Man_power[$i]->company_name}}">{{$Man_power[$i]->company_name}}</td>
+                                    <td class="man_date" data-text="{{$Man_power[$i]->man_date}}">{{$Man_power[$i]->man_date}}</td>
+                                    <td class="worker_name" data-text="{{$Man_power[$i]->worker_name}}">{{$Man_power[$i]->worker_name}}</td>
+                                    <td class="worker_key" data-text="{{$Man_power[$i]->worker_id}}">{{$Man_power[$i]->worker_key}}</td>
+                                    <td class="man_hours" data-text="{{$Man_power[$i]->man_hours}}">{{$Man_power[$i]->man_hours}}</td>
 
 
                                 </tr>
@@ -182,6 +182,26 @@
     </div>
     
 </div>
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit Project</h5>
+          <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="container-fluid" id="modal-content">
+
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" id="update_btn" onclick="edit_project('update')">Update</button>
+          <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 <script>
@@ -190,10 +210,124 @@
     let total = JSON.parse(projects);
     workers = JSON.parse(workers);
     let flag_supervisor = 0;
+
+    let summary_id = -1 ; 
+    let present_table = '';
+    let selects = {
+            'company_value' : [1,2,3,4,5,6,7,8],
+            'p_status':['active','work Done',"aborted","fully paid"],
+            'p_type':['Main cone','Sub-con'],
+            'p_work':['Steel Trusses','Steel Trusses & Roofing','Fbarication Only','Fbarication & Install','Others'],
+            'p_ic':['WYF','WKL','WkM','CMH','Other'],
+            'include_materials':['YES','NO'],
+            'include_roofing' : ["YES","NO"],
+            'p_award':['Letter of Award','Approved Quotation','Purchase Order','Verbally','None'],
+            'f_completion' : ['0%','10%','20%','30%','40%','50%','60%','70%','80%','90%','100%'],
+            'p_completion' : ['0%','10%','20%','30%','40%','50%','60%','70%','80%','90%','100%'],
+            'sub_description' : ['Purchasing Materials','Crane','Lorry','Referer Fee','Painting','Wages','Others']
+        }
     $(document).ready(function(){
-        $("#table_hours").DataTable().order([3,'desc']).draw();
+        $("#table_hours").DataTable({
+            searching :false,
+            "lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "All"]]
+        }).order([3,'desc']).draw();
         $("#table_projectsummary").DataTable();
     })
+
+    let present_Id;
+    function editRow(id) {
+        
+        let disableEdit = [];
+        let deleteEdit = ['progress_total','total_work_done','amount_due_to_claim','p_name','company_name',"worker_name"];
+        present_table = id.split("-")[0];
+        present_Id = id.split("-")[1];
+        let edit_status = $("#"+id);
+        let contenthtml = "";
+        // $("#"+id).toggleClass("edit_status");
+
+        //make p_code select
+        
+        
+        $("#"+id+" td").filter(function(){
+            //show create,update,delte button show
+           console.log($(this).data('text'));
+            let columnClass = $(this).attr('class').split(" ")[0];
+
+            if ($("."+columnClass+"-header").html().search("upload") == -1) {
+                contenthtml += "<div class='row' style = 'margin-top:10px'>";
+                contenthtml += "<div class='col-md-6'>"
+                contenthtml += "<label>";
+                contenthtml += $("."+columnClass+"-header").html();
+                contenthtml += "</label>"
+                contenthtml += "</div>"
+                
+                contenthtml += "<div class='col-md-6'>"
+                if (selects[columnClass] !== null && selects[columnClass] !== undefined) {
+                    contenthtml += "<select class='form-control edit_set' id='edit-"+columnClass+"'>";
+                        console.log(columnClass);
+                    for (let k = 0; k < selects[columnClass].length; k++) {
+                        if (selects[columnClass][k] == $(this).data('text')) {
+                            contenthtml += "<option selected>"+selects[columnClass][k]+"</option>"
+                        }else{
+                            contenthtml += "<option>"+selects[columnClass][k]+"</option>"
+                        }
+                    }
+                    contenthtml += "</select>";
+
+                }else if(columnClass == "p_code"){
+                    let p_codes = [];
+                    $("#table_projectsummary-tbody .p_code").filter(function(){
+                        p_codes[$(this).text()] = 0;
+                    })
+                    let select_p_codes = "<select class='form-control edit_set' id= 'edit-p_code'>";
+                    for(p_code in p_codes)
+                    {
+                        if (p_code == $(this).data('text')) {
+                            select_p_codes += "<option selected>"+p_code+"</option>";  
+                        }else{
+                            select_p_codes += "<option>"+p_code+"</option>";
+                        }
+                    }
+                    select_p_codes += "</select>";
+                    contenthtml += select_p_codes;
+                }else if(columnClass == "worker_key"){
+                    let select_worker_codes = "<select class='form-control edit_set' id= 'edit-worker_key'>"
+                    for (let k = 0; k < workers.length; k++) {
+                        if (workers[k]['worker_id'] == $(this).data('text')) {
+                            select_worker_codes += "<option selected value='"+workers[k]['worker_id']+"'>"+workers[k]['worker_name']+"</option>";  
+                        }else{
+                            select_worker_codes += "<option value='"+workers[k]['worker_id']+"'>"+workers[k]['worker_name']+"</option>";
+                        }
+                    }
+                    select_worker_codes += "</select>";
+                    contenthtml += select_worker_codes;
+                }
+                else{
+                    let inputtype = 'text';
+                    if (columnClass.search("date") != -1) {
+                        inputtype = 'date'
+                    }
+                    console.log($(this).data('text'));
+                    contenthtml += "<input type='"+inputtype+"' class='form-control edit_set' id='edit-"+columnClass+"' value='"+$(this).data('text')+"'>";
+
+                    
+                }
+                contenthtml += "</div>"
+                contenthtml += "</div>";
+            };
+            
+
+        })
+        $("#modal-content").html(contenthtml);
+        $("#exampleModal").modal('show');
+        //disable edit column
+        for (let i = 0; i < disableEdit.length; i++) {
+            $("#edit-"+disableEdit[i]).attr('disabled','');            
+        }
+        for (let i = 0; i < deleteEdit.length; i++) {
+            $("#edit-"+deleteEdit[i]).parent().parent().remove()
+        }
+    }
     function getvisor() {
         let worker_id = $("#worker_name").val();
         let p_code = $("#p_code").val();
@@ -268,6 +402,41 @@
                 flag_supervisor : flag_supervisor
             },
             success : function(data){
+                toastFunction()
+            }
+        })
+    }
+
+    function edit_project(editType) {
+        let editData = {};
+        let p_code = $("#edit-p_code").val();
+        let date = $("#edit-man_date").val();
+        let worker_id = $("#edit-worker_key").val();
+        let man_hours = $("#edit-man_hours").val();
+        let p_name = $("#edit-p_name").val();
+        let company_name = $("#edit-companu_name").val();
+        editData['man_date'] = date;
+        editData['man_workerid'] = worker_id;
+        editData['man_hours'] = man_hours;
+        editData['p_code'] = p_code;
+        console.log(editData);
+        $.ajax({
+            type: 'post',
+            url: "{{url('/manpower/edit')}}",
+            data: {
+                editType: editType,
+                present_Id: present_Id,
+                editData: editData
+            },
+            success: function (data) {
+                // displaymans();  
+                $("#"+present_table+"-"+present_Id+" td").filter(function(){
+                let columnClass = $(this).attr('class').split(" ")[0];
+                $(this).text(editData[columnClass]);
+                $(this).attr("data-text",editData[columnClass])
+                // $(this).data('text') = result[columnClass]
+            })
+            console.log("update"); 
                 toastFunction()
             }
         })
